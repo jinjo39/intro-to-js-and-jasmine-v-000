5cb3935d5ccda87c4fe9016287b5ac28f27a6652
jinjo39 <c.delavar@gmail.com>
2016-04-08 23:32:04 -0700

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(iceCream){
+  return "I love " + iceCream;
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth(){
+  return true;
+}
